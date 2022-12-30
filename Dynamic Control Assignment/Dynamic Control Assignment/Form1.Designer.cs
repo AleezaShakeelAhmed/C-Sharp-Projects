9a16cd6f73bd293ee2b8fd49e3a0f47cc72a0efe
Aleeza Shakeel Ahmed <115490076+AleezaShakeelAhmed@users.noreply.github.com>
2022-12-30 15:36:49 +0500

    .

---
+++ Dynamic Control Assignment/Dynamic Control Assignment/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Dynamic_Control_Assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtmaininput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.sumbton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(12, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(497, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter The Number How Many Number You Want To Add";
+            // 
+            // txtmaininput
+            // 
+            this.txtmaininput.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtmaininput.Location = new System.Drawing.Point(515, 61);
+            this.txtmaininput.Multiline = true;
+            this.txtmaininput.Name = "txtmaininput";
+            this.txtmaininput.Size = new System.Drawing.Size(171, 28);
+            this.txtmaininput.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Info;
+            this.label2.Location = new System.Drawing.Point(25, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(223, 35);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Addition Calculator";
+            // 
+            // btnNext
+            // 
+            this.btnNext.BackColor = System.Drawing.SystemColors.Info;
+            this.btnNext.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNext.Location = new System.Drawing.Point(703, 59);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(85, 28);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = false;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // sumbton
+            // 
+            this.sumbton.BackColor = System.Drawing.SystemColors.Info;
+            this.sumbton.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sumbton.Location = new System.Drawing.Point(30, 371);
+            this.sumbton.Name = "sumbton";
+            this.sumbton.Size = new System.Drawing.Size(85, 30);
+            this.sumbton.TabIndex = 4;
+            this.sumbton.Text = "Sum";
+            this.sumbton.UseVisualStyleBackColor = false;
+            this.sumbton.Click += new System.EventHandler(this.sumbton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Dynamic_Control_Assignment.Properties.Resources.calculator23;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.sumbton);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtmaininput);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Calculate Addition";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtmaininput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button sumbton;
+    }
+}
+
---
+++ Dynamic Control Assignment/Dynamic Control Assignment/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dynamic_Control_Assignment
+{
+    public partial class Form1 : Form
+    {
+        public int total = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            total = int.Parse(txtmaininput.Text);
+            AddBoxes(total);
+        }
+        public  void AddBoxes(int input1)
+        {
+            int y_Add = 39;
+            for (int i = 1; i <= input1; i++)
+            {
+                TextBox newcreated = new TextBox();
+                newcreated.Name = "txtsubject" + i;
+                newcreated.Location = new Point(515, (69+y_Add));
+                newcreated.Size = new Size(171, 28);
+                newcreated.Font = new Font("Times New Roman", 14, FontStyle.Bold);
+                this.Controls.Add(newcreated);
+                y_Add += 39;
+            }
+        }
+
+        private void sumbton_Click(object sender, EventArgs e)
+        {
+            int sum = 0;
+            for (int i = 1; i <= total; i++)
+            {
+                sum =sum + int.Parse(this.Controls["txtsubject"+i].Text);
+            }
+            MessageBox.Show("The Sum is Equal To = "+sum.ToString());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
